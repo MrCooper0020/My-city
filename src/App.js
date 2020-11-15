b8a3faf68f4de088176036945d43f830e83c8d45
MrCooper0020 <matheushp.com@hotmail.com>
2020-11-15 19:29:18 -0300

    config firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Firebase from "./services/firebase-connect";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	Firebase.auth()
+		.signInWithEmailAndPassword("1117956@imed.edu.br", "123456")
+		.then((retorno) => {
+			console.log("Usuário Logado: " + retorno.user.uid);
+		})
+		.catch((erro) => {
+			console.log(erro);
+		});
+
+	return <div className="App"></div>;
 }
 
 export default App;
