ba00f19649316fc523abea19897b3895b593642b
MrCooper0020 <matheushp.com@hotmail.com>
2020-11-15 18:37:54 -0300

    first commit

---
+++ src/services/firebase-config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD_jB6go7LYdJqjesa2D_uVn3vlLTOpXkw",
+  authDomain: "repair-service-web.firebaseapp.com",
+  databaseURL: "https://repair-service-web.firebaseio.com",
+  projectId: "repair-service-web",
+  storageBucket: "repair-service-web.appspot.com",
+  messagingSenderId: "1002218386478",
+  appId: "1:1002218386478:web:edf481a04c705279791bbf"
+};
+
+// Initialize Firebase
+export default firebaseConfig;
---
+++ src/services/firebase-connect.js
@@ -0,0 +1,6 @@
+import Firebase from "firebase";
+import FireBaseConfig from "./firebase-config";
+
+Firebase.initializeApp(FireBaseConfig);
+
+export default Firebase;
